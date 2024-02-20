50258381cd02b7a9bb81774c0f2f912c984e5145
Khalil <s204161@student.dtu.dk>
2024-02-20 18:01:29 +0100

    classifier free ran complete, mix non-human and human

--- ADLCV_Exercise_4/ex6/sample_cFg.py
+++ ADLCV_Exercise_4/ex6/sample_cFg.py
@@ -28,3 +28,12 @@ x_new = ddpm_cFg.p_sample_loop(unet_ddpm_cFg, 5, y=y)
 imgs = [im_normalize(tens2image(x_gen.cpu())) for x_gen in x_new]
 show(imgs, fig_titles=CLASS_LABELS, title='classifier FREE guidance', save_path='assets/cFg_samples.png')
 
+# Sample but with mixed labels
+# fill y with 0. values in 5x5 tensor
+y = torch.zeros(5, 5, device=device)
+for i in range(5):
+    y[i,0] = 0.75 - i*0.25
+    y[i,1] = 0.25 + i*0.25
+x_new = ddpm_cFg.p_sample_loop(unet_ddpm_cFg, 5, y=y)
+imgs = [im_normalize(tens2image(x_gen.cpu())) for x_gen in x_new]
+show(imgs, fig_titles=['100% human', '75%, 25%', '50%, 50%', '25%, 75%','100% non-human'], title='classifier FREE guidance', save_path='assets/cFg_samples_mix_human_non_human.png')
